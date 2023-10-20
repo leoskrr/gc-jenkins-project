e9fc3014dc2f95c4ea7f939d9210066ccfd8f10d
Leonardo Viana <vianaleonardo.es@gmail.com>
2023-10-20 00:31:27 -0400

    add extra test cases

--- src/operations.test.ts
+++ src/operations.test.ts
@@ -1,8 +1,16 @@
 import { sum, sub } from './operations';
 
 describe("#sum", () => {
-    it("should sum numbers properly", () => {
+    it("should sum positive numbers properly", () => {
         expect(sum(1, 1)).toBe(2);
+    });
+
+    it("should sum negative numbers properly", () => {
+        expect(sum(-1, -1)).toBe(-2);
+    });
+
+    it("should sum a negative and a positive number properly", () => {
+        expect(sum(-1, 2)).toBe(1);
     });
 });
 
