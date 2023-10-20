15d21a73bf722e17ce750a7f60a91b49b6fb8351
Leonardo Viana <vianaleonardo.es@gmail.com>
2023-10-20 00:06:26 -0400

    add build phase

--- src/operations.ts
+++ src/operations.ts
@@ -5,3 +5,6 @@ export function sum(a: number, b: number): number {
 export function sub(a: number, b: number): number {
     return a - b;
 }
+
+console.log("### Resultados ###")
+console.log(`A soma de 1 + 1 eh ${sum(1, 1)} e a subtração de 2 - 1 eh ${sub(2, 1)}`)
